3cbd2eab8af67441c0b4bb6b9bf118dc66291e4c
userNIGAM <nigam333nigam@gmail.com>
2026-08-16 22:17:53 +0545

    added winner to sidebar

--- frontend/src/admin/index.jsx
+++ frontend/src/admin/index.jsx
@@ -7,6 +7,7 @@ import Sponsors from "./pages/Sponsors";
 import SupportedBy from "./pages/SupportedBy";
 import Countdown from "./pages/Countdown";
 import Awards from "./pages/Awards";
+import WinnersAdmin from "./components/Winner";
 
 export default function AdminRoutes() {
   return (
@@ -20,6 +21,9 @@ export default function AdminRoutes() {
 
         <Route path="countdown" element={<Countdown />} />
         <Route path="awards" element={<Awards />} />
+        <Route path="winners" element={<WinnersAdmin />} />
+
+
       </Route>
     </Routes>
   );
